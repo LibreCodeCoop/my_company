3b03a24dc41380c9161a49074a98f7d23da9f7af
Vitor Mattos <vitor@php.rio>
2023-07-11 23:56:12 -0300

    Remove webpack.common.config.js and make compatible with docker
    Using docker the page don't refresh after save a VueJS file, but now,
    after this change, will refresh
    
    Is necessary to expose the port 3000 in your docker environment
    
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,12 @@ const { merge } = require('webpack-merge')
 
 const nextcloudWebpackConfig = require('@nextcloud/webpack-vue-config')
 
-const commonWebpackConfig = require('./webpack.common.config.js')
-
-module.exports = merge(nextcloudWebpackConfig, commonWebpackConfig)
+module.exports = merge(nextcloudWebpackConfig, {
+	optimization: process.env.NODE_ENV === 'production'
+		? { chunkIds: 'deterministic' }
+		: {},
+	devServer: {
+		port: 3000, // use any port suitable for your configuration
+		host: '0.0.0.0', // to accept connections from outside container
+	}
+})
