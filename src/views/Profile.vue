3af5255c923dd69083419cbe178a914d108856a0
Vitor Mattos <vitor@php.rio>
2023-07-14 20:42:45 -0300

    Add more default fields
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -8,6 +8,12 @@
 			<div class="personal-settings-setting-box">
 				<EmailSection />
 			</div>
+			<div class="personal-settings-setting-box">
+				<PhoneSection />
+			</div>
+			<div class="personal-settings-setting-box">
+				<BiographySection />
+			</div>
 		</div>
 	</div>
 </template>
@@ -16,6 +22,8 @@
 import AvatarSection from 'settings/components/PersonalInfo/AvatarSection.vue'
 import DisplayNameSection from 'settings/components/PersonalInfo/DisplayNameSection.vue'
 import EmailSection from 'settings/components/PersonalInfo/EmailSection/EmailSection.vue'
+import PhoneSection from 'settings/components/PersonalInfo/PhoneSection.vue'
+import BiographySection from 'settings/components/PersonalInfo/BiographySection.vue'
 
 export default {
 	name: 'Profile',
@@ -24,6 +32,8 @@ export default {
 		AvatarSection,
 		DisplayNameSection,
 		EmailSection,
+		PhoneSection,
+		BiographySection,
 	},
 }
 </script>
