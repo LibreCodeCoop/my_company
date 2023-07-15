5d9ab056ca2605d434aeb94076e653530b81408a
Vitor Mattos <vitor@php.rio>
2023-07-14 21:59:37 -0300

    Add security view
    Signed-off-by: Vitor Mattos <vitor@php.rio>

---
+++ src/views/Security.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<div id="personal-settings">
+			<h2 class="hidden-visually">{{ t('my_company', 'Security') }}</h2>
+			<PasswordSection />
+		</div>
+	</div>
+</template>
+
+<script>
+import PasswordSection from 'apps/settings/src/components/PasswordSection.vue'
+
+export default {
+	name: 'Profile',
+
+	components: {
+		PasswordSection,
+	},
+}
+</script>
