09aebadf167fbf3ce127dca659a4832d8f5efed1
Vitor Mattos <vitor@php.rio>
2023-08-18 03:38:39 -0300

    Fix route
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- src/components/LeftSidebar.vue
+++ src/components/LeftSidebar.vue
@@ -33,7 +33,7 @@ export default {
 	},
 	data() {
 		return {
-			approved: loadState('my_company', 'approved', false),
+			approved: loadState('my_company', 'registration-approved', false),
 		}
 	},
 }
